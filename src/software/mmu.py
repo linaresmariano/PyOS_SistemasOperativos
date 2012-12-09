53ebde92370d34d5b6e5655cc4459f47ff2a1f52
Leandro Gomez <leandro.gomez73@yahoo.com.ar>
2012-12-09 17:28:55 -0300

    Creo que paginacion esta terminado, pero no logro hacer que ande con el Main.
    Tal vez el error esta en el 'fetchInstruction' que usa el CPU, solo le
    da el PCB (o su id) cuando deberia darle el pc tambien

--- src/software/mmu.py
+++ src/software/mmu.py
@@ -4,29 +4,50 @@ Created on 08/12/2012
 @author: leandro
 '''
 
-from page_table import PageTable
+from page_table import PageTable, SwappedPageException
 
 class MMU:        
+    
+    def setKernel(self,kernel):
+        self.kernel = kernel
+        
     def __init__(self, memory, hdd, page_table):
         self.memory = memory
         self.hdd = hdd
-        self.table = page_table
+        self.page_table = page_table
         
     # Public interface
     
-    def fetchInstruction(self, aPCBId, pc):
-        page = pc / self.getLenBlock(aPCBId)
-        desp = pc % self.getLenBlock(aPCBId)
-        base = self.getPageTable().baseByPCBId(aPCBId, page)
-        
+    
+#    def fetchInstruction(self, aPCBId, pc):
+#        page = pc / self.getLenBlock(aPCBId)
+#        desp = pc % self.getLenBlock(aPCBId)
+#        try:
+#            base = self.getPageTable().baseByPCBId(aPCBId, page)
+#        except SwappedPageException:
+#            self.unswap(aPCBId, page)
+#            return self.fetchInstruction(aPCBId, pc)
+#        inst = self.getMemory().read(base + desp)
+#        
+#        return inst
+    
+    def fetchInstruction(self, aPCB):
+        pcb = aPCB
+        pc = pcb.getPc()
+        page = pc / self.getLenBlock()
+        desp = pc % self.getLenBlock()
+        try:
+            base = self.getPageTable().getBase(aPCB, page)
+        except SwappedPageException:
+            self.unswap(aPCB, page)
+            return self.fetchInstruction(aPCB, pc)
         inst = self.getMemory().read(base + desp)
         
-        return inst
+#        return inst        
     
     def swap(self):
         '''
-        ¿No tiene parametros? 
-        ¿Que hace?
+        Que hace
         '''
         pass
     
@@ -104,6 +125,27 @@ class Paginacion(MMU):
         MMU.__init__(self, memory, hdd, page_table)
         self.lenBlock = lenBlock
         
+    def load(self, aPCB, insts):
+        '''
+        Try to associate a page of a process to a page in memory.
+        If there is not place, the rest process pages are swapped.
+        Precondition: have called addPCB() method of Table()
+        '''
+        paged_instrs = self.paginate(insts)
+        free_memory_pages_bases = self.getFreeMemoryPagesBases()
+        
+        virtual_paged_instrs = paged_instrs[:]
+        
+        for free_page_base in free_memory_pages_bases:
+            page = virtual_paged_instrs.pop(0)
+            self.getPageTable().setPage(aPCB , paged_instrs.index(page) , free_page_base , len(page), False)
+            self.writeOnMemory(page, free_page_base)
+            if(not virtual_paged_instrs):
+                break
+            
+        for to_swapped_pages in virtual_paged_instrs:
+            self.getHDD().swapPageToPCB(aPCB, paged_instrs.index(to_swapped_pages), to_swapped_pages)
+        
     def getLenBlock(self):
         '''
         Return the size of a page
@@ -114,28 +156,26 @@ class Paginacion(MMU):
         '''
         return the number of pages needed to specific PCB
         '''
-        self.getLenPCB(aPCB) / self.getLenBlock(aPCB) + 1
+        return self.getLenPCB(aPCB) / self.getLenBlock() + 1
         
-    def load(self, aPCB, insts):
+    def numPagesFor(self, aPCB):
+        return self.calcNumPages(aPCB)
+    
+    def writeOnMemory(self, page, free_page_number):
         '''
-        
+        Write all instructions on 'page' from 'free_page_number'.
+        If there are any data, this is overwrited.
         '''
-        paged_instrs = self.page(insts)
-        free_memory_pages = self.getFreeMemoryPages()
-        
-        virtual_paged_instrs = paged_instrs[:]
-        
-        for free_page in free_memory_pages:
-            page = virtual_paged_instrs.pop()
-            self.getPageTable().addPage(aPCB , paged_instrs.index(page) , free_page , len(page), False)
-            self.load_on_memory(page, free_page)
-            if(not virtual_paged_instrs):
-                break
+        count = 0
+        for inst in page:
+            self.getMemory().write(free_page_number + count, inst)
+            count += 1
             
-        for to_swapped_pages in virtual_paged_instrs:
-            self.getHDD().swap_to_pcb(aPCB, paged_instrs.index(to_swapped_pages ), to_swapped_pages)
-            
-    def page(self, instrs):
+    def paginate(self, instrs):
+        '''
+        Make a list of instructions to a list of list of instructions, 
+        the length each list of instructions is the length of a page. 
+        '''
         paged_instrs = []
         temp_page = []
         for instr in instrs:
@@ -144,26 +184,79 @@ class Paginacion(MMU):
                 paged_instrs.append(temp_page)
                 temp_page = []
                 
+        if(len(temp_page) > 0):
+            paged_instrs.append(temp_page)        
+        
         return paged_instrs
     
-    def getFreeMemoryPages(self):
+    def getFreeMemoryPagesBases(self):
+        '''
+        Return the free pages on memory
+        '''
         all_table_units = self.getPageTable().getTable().values()
         all_tables_unit_pages = []
         for table_unit in all_table_units:
             all_tables_unit_pages.extend(table_unit.getPages().values())
             
-        base_ocupied_pages = [ page.getBase() for page in all_tables_unit_pages if not page.swapped() ]
+        base_ocupied_pages = [ page.getBase() for page in all_tables_unit_pages if not page.isSwapped() ]
         all_base_memory_pages = self.getMemoryPagesBases()
         free_pages = [x for x in all_base_memory_pages if not (x in base_ocupied_pages)]
         return free_pages
     
     def getMemoryPagesBases(self):
+        '''
+        return all bases to a specific memory size
+        '''
         memory_size = self.getMemory().size()
         pages = memory_size / self.getLenBlock()
         return [ page * self.getLenBlock() for page in range(pages) ]
+         
+    
+    def unswap(self, aPCB, page_number):
+        '''
+        Load in memory the page that is in HDD.
+        '''
+        instrs = self.getHDD().getSwappedPage(aPCB, page_number)
+        free_memory_page = self.getForcedFreePage()
+        self.getPageTable().setPage(aPCB,page_number,free_memory_page,len(instrs),False)
+        self.writeOnMemory(instrs, free_memory_page)
         
+    def getForcedFreePage(self):
+        '''
+        if there is any free page on memory, return it. unless,
+        swap some page of some pcb and returns its memory page.
+        '''
+        free_bases = self.getFreeMemoryPagesBases()
+        if not free_bases:
+            self.swapSomePCB()
+            free_bases = self.getFreeMemoryPagesBases()
+            
+        return free_bases[0] 
+    
+    def swapSomePCB(self):
+        '''
+        swap some pcb (should be like 'banquero') and return this free page
+        '''
+        all_process = self.getPageTable().keys()
+         
+        
+        for process in all_process:
+            if not self.getPageTable().isBlocked(process):
+                table_unit = self.getPageTable().getTable()[process]
+                no_swapped_pages_number = [ page_number for page_number in table_unit.getPages().keys() if not table_unit.isSwapped(page_number) ]
+                page_number = no_swapped_pages_number[0]
+                page = table_unit.getPages()[page_number]
+                base = page.getBase()
+                limit = page.getLimit()
+                table_unit.swap(page_number)
+                instrs = [ self.getMemory().read(base+ints_index) for ints_index in range(limit) ]
+                self.getHDD().swapPageToPCB(process, page_number, instrs)
+                return base
+            
+        raise Exception("Maybe this is a bug")        
+    
 # Exceptions
 
-class  AbstractMethodException(Exception):
+class AbstractMethodException(Exception):
     def __str__(self):
         return "This method must be implemented on sub-class"
